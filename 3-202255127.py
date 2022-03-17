6c864dea0e5ca6e80b12630a7614f34705d1e2a7
munmoom <100830462+munmoom@users.noreply.github.com>
2022-03-18 08:56:08 +0900

    Colaboratory를 통해 생성됨

---
+++ 3-202255127.py
@@ -0,0 +1,62 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "name": "Untitled6.ipynb",
+      "provenance": [],
+      "authorship_tag": "ABX9TyPZAehX8h9Y3L1VMw4WEDG/",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/munmoom/First-week-BCP/blob/main/3-202255127.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "execution_count": 7,
+      "metadata": {
+        "colab": {
+          "base_uri": "https://localhost:8080/"
+        },
+        "id": "UubeuTB2SBnq",
+        "outputId": "48444f2e-1113-4947-b822-4a360c8d2842"
+      },
+      "outputs": [
+        {
+          "output_type": "stream",
+          "name": "stdout",
+          "text": [
+            "-123 -47 35\n",
+            "-135\n",
+            "-45.0\n"
+          ]
+        }
+      ],
+      "source": [
+        "a, b, c = input().split()\n",
+        "a = int(a)\n",
+        "b = int(b)\n",
+        "c = int(c)\n",
+        "print (a+b+c)\n",
+        "print((a+b+c)/3)\n",
+        "\n"
+      ]
+    }
+  ]
+}
